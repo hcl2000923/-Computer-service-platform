48d3cfed7a12ad2b30d0ce5caf9b92a04dd38f26
hcl0923 <2958731272@qq.com>
2021-06-03 17:06:48 +0800

    Signed-off-by: hcl0923 <2958731272@qq.com>

---
+++ PCService/shop-index/src/test/java/com/yc/pcservice/PcServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yc.pcservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
