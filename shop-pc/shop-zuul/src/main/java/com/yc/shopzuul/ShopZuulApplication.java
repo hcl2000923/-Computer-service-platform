896749449b4e5a5aa490a4e66cf0e4c575e4c309
hcl0923 <2958731272@qq.com>
2021-06-03 22:18:14 +0800

    Signed-off-by: hcl0923 <2958731272@qq.com>

---
+++ shop-pc/shop-zuul/src/main/java/com/yc/shopzuul/ShopZuulApplication.java
@@ -0,0 +1,15 @@
+package com.yc.shopzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ShopZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopZuulApplication.class, args);
+    }
+
+}
